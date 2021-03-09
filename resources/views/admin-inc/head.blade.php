f1e72041fe0b709f1ea7c9bf68871a255a083549
Yesho Simon <78914267+yesho-a@users.noreply.github.com>
2021-03-09 13:21:50 +0300

    Update head.blade.php

--- resources/views/admin-inc/head.blade.php
+++ resources/views/admin-inc/head.blade.php
@@ -11,7 +11,6 @@
 
   
   <!-- Styles -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('assets') }}/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
