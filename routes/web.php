eabe90692f0865f1482fe07e5d62046a8d56a5fd
Yesho Simon <78914267+yesho-a@users.noreply.github.com>
2022-08-08 20:04:50 +0300

    Update web.php
    added return route

--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,10 @@ Route::get('pdf','App\Http\Controllers\TenantController@generatePDF');
 
 Route::get('dropzone', [DropzoneController::class, 'dropzone']);
 Route::post('dropzone/store', [DropzoneController::class, 'dropzoneStore'])->name('dropzone.store');
+
+
+Route::get('/return', function () {
+   return "Hello welcome to my website";
+
+      
+});
